a177e0ab6976939f11fb67a4d3588b5a06294461
Miguel Machado <miiguellmp@gmail.com>
2019-05-17 22:33:48 -0300

    fetching data from an api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
 import React, { Component } from "react";
+import axios from "axios";
 import SearchBar from "./components/SearchBar";
 
 export default class App extends Component {
-  onSearchSubmit(term) {
-    console.log(term);
+  async onSearchSubmit(term) {
+    const response = await axios.get("https://api.unsplash.com/search/photos", {
+      params: { query: term },
+      headers: {
+        Authorization:
+          "Client-ID a8dcf6b18aeb5c39a893a344dd47143cffbe6808ac2a1ceb656cbfb3b5962b03"
+      }
+    });
+
+    console.log(response.data.results);
   }
 
   render() {
