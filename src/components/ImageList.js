1940f0fd6b5ac7bc566acb0cc2f14818f9ffc060
Miguel Machado <miiguellmp@gmail.com>
2019-05-17 23:54:23 -0300

    importing image card component and passing the images thru a prop to the card

--- src/components/ImageList.js
+++ src/components/ImageList.js
@@ -1,15 +1,19 @@
 import React from "react";
+import ImageCard from "./ImageCard";
+import "./ImageList.css";
 
 const ImageList = props => {
   // images is an array from the axios response
   // for each item in this array we want to return a img
   // images = response.data.results (app.js)
   // destructuring the props
-  const images = props.images.map(({ alt_description, id, urls }) => {
-    return <img key={id} src={urls.regular} alt={alt_description} />;
+
+  // passing the images thru a prop to the card so the ImageCard can access the images url
+  const images = props.images.map(image => {
+    return <ImageCard key={image.id} imageprop={image} />;
   });
 
-  return <div>{images}</div>;
+  return <div className="image-list">{images}</div>;
 };
 
 export default ImageList;
